7f128bc42e45324b5a6c2e4e98a01bd56cc55598
maykoestevez <MaykoYefree.EstevezPena@iwgplc.com>
2023-05-25 22:01:09 -0400

    Adding product details update

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { ProductService } from '../product.service';
 import { Product } from '../product';
 import { Observable } from 'rxjs';
+import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-details',
@@ -12,7 +15,20 @@ import { Observable } from 'rxjs';
 export class ProductDetailsComponent implements OnInit {
 
   $product!: Observable<Product>;
-  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+  productForm!: FormGroup;
+  constructor(private productService: ProductService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder) {
+    this.productForm = formBuilder.group({
+      id: [0, Validators.required],
+      name: ['', Validators.required],
+      description: [''],
+      price: [null, Validators.required],
+      category: [''],
+      quantity: ['', Validators.required]
+    })
+  }
 
   ngOnInit(): void {
     const id = this.route.snapshot.params['id'] | 0;
@@ -20,7 +36,21 @@ export class ProductDetailsComponent implements OnInit {
   }
 
   getProductById(productId: number) {
-    this.$product = this.productService.getProductById(productId);
+    this.productService
+      .getProductById(productId)
+      .subscribe(product => this.productForm.setValue(product));
+
+  }
+
+  async save() {
+    try {
+      const product = this.productForm.value;
+      await this.productService.updateProduct(product).toPromise();
+      this.router.navigate(['product-list']);
+    } catch (error) {
+      alert(error);
+    }
+
   }
 
 }
